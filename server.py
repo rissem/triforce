df6e20ba469d182d90c379e253d17ae3a322bed6
Mike Risse <rissem@gmail.com>
2019-01-09 21:51:57 -0800

    chord recognition

--- server.py
+++ server.py
@@ -2,21 +2,26 @@ import serial
 from random import random
 import time
 from flask import Flask
+from flask_cors import CORS
 from flask import request
+from music21 import chord
 
 app = Flask(__name__)
+CORS(app)
 
 ser = serial.Serial('/dev/tty.usbmodem14201', 115200, timeout=1)
 
 @app.route("/")
 def hello():
-    if request.args.get('msg') == "144": 
-        print("DO STUFF " + request.args.get('msg'))
-        colors = [str(int(random() * 255)) for i in range(0, 4*3)]
-        string = "<" + ",".join(colors) + ">"
-        # ser.write(b'<255,255,255,255,255,255,255,255,255,255,255,255>')
+    note_nums = [int(x) for x in request.args.get("notes").split("_")]
+
+    colors = [str(int(random() * 255)) for i in range(0, 4*3)]
+    string = "<" + ",".join(colors) + ">"
+    # ser.write(b'<255,255,255,255,255,255,255,255,255,255,255,255>')
+
+    if 'triangle' in request.args:
         print("WRITING BYTES")
         ser.write(bytes(string, 'utf-8'))
-        return "HI"
-    return "YO"
+    result = chord.Chord(note_nums)
 
+    return result.root().name + " " + result.commonName, 200
--- triangle.js
+++ triangle.js
@@ -1,17 +1,74 @@
-console.log('hello world')
+const heldNotes = {}
+let pedalNotes = {}
+let pedal = false
+
+const noteDown = note => {
+  heldNotes[note] = 1
+  if (pedal) {
+    pedalNotes[note] = 1
+  }
+
+  console.log('SUSTAINED NOTES', sustainedNotes())
+  fetch(
+    'http://localhost:5000?triangle=true&notes=' + Object.keys(sustainedNotes()).join('_'),
+    {}
+  ).then(res => {
+    res.text().then(text => {
+      document.getElementById('chord').innerHTML = text.replace(/([A-Z])-/g, function(x, y) {
+        console.log(arguments)
+        return y + 'b'
+      })
+    })
+  })
+}
+
+const noteUp = note => {
+  delete heldNotes[note]
+  if (sustainedNotes.length == 0) return
+  // fetch('http://localhost:5000?notes=' + Object.keys(sustainedNotes()).join('_'), {}).then(res => {
+  //   res.text().then(text => {
+  //     document.getElementById('chord').innerHTML = text
+  //   })
+  // })
+}
+
+const pedalDown = () => {
+  pedal = true
+  Object.assign(pedalNotes, heldNotes)
+}
+
+const pedalUp = () => {
+  pedal = false
+  pedalNotes = {}
+}
+
+const sustainedNotes = () => {
+  const sustainedNotes = {}
+  Object.assign(sustainedNotes, pedalNotes)
+  Object.assign(sustainedNotes, heldNotes)
+  return sustainedNotes
+}
 
 navigator.requestMIDIAccess().then(access => {
-    const inputs = access.inputs.values()
-    const outputs = access.outputs.values()
-    const input = inputs.next()
-    if (input.value) {
-        input.value.onmidimessage = function(event) {
-            const [msg, d1, d2] = event.data
-            if (msg == 176 && d1 == 66 && d2 == 127) {
-                playPause(event)
-            }
-            console.log('MIDI EVENT', event)
-            fetch(`http://localhost:5000?msg=${msg}&d1=${d1}&d2=${d2}`, { mode: 'no-cors' })
-        }
+  const inputs = access.inputs.values()
+  const outputs = access.outputs.values()
+  const input = inputs.next()
+  if (input.value) {
+    input.value.onmidimessage = function(event) {
+      const [msg, d1, d2] = event.data
+      console.debug(msg, d1, d2)
+      if (msg == 144) {
+        noteDown(d1)
+      }
+      if (msg == 128) {
+        noteUp(d1)
+      }
+      if (msg == 176 && d2 == 127) {
+        pedalDown()
+      }
+      if (msg == 176 && d2 == 0) {
+        pedalUp()
+      }
     }
+  }
 })
